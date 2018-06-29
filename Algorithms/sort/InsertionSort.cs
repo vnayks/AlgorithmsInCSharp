8f452d1c300876d5f4058aba80a611f754f4af34
vnayks <meetvince@gmail.com>
2018-06-29 16:18:16 -0500

    Update InsertionSort.cs

--- Algorithms/sort/InsertionSort.cs
+++ Algorithms/sort/InsertionSort.cs
@@ -4,25 +4,31 @@ namespace Algorithms {
 
     public static class InsertionSort {
 
-        public static int[] Sort (int[] nums) {
+        public static int[] Sort (int[] nums, SortOrder sortOrder = SortOrder.Ascending) {
 
             if (nums == null || nums.Length <= 1)
                 return nums;
 
             for (var i = 1; i < nums.Length; i++) {
                 var key = nums[i];
-                var j = i-1;
+                var j = i - 1;
 
-                while (j >= 0 && key < nums[j]) {
-                    nums[j + 1] = nums[j];
-                    --j;
+                if (sortOrder == SortOrder.Ascending) {
+                    while (j >= 0 && key < nums[j]) {
+                        nums[j + 1] = nums[j];
+                        --j;
+                    }
+                } else {
+                    while (j >= 0 && key > nums[j]) {
+                        nums[j + 1] = nums[j];
+                        --j;
+                    }
                 }
 
                 nums[j + 1] = key;
             }
 
             return nums;
-
         }
 
     }
